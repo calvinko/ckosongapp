e0c4d18231004307c2caa8172f28335e742e09cc
cko <ko.calvin@gmail.com>
2026-03-02 16:36:17 -0800

    add songs

--- lib/scripts/writeSongListWithContent.ts
+++ lib/scripts/writeSongListWithContent.ts
@@ -5,7 +5,7 @@ import { FullMetaWithContent } from '../../ts/types/songMeta.interface';
 import { getAllSongsWithContent } from "../songs/fetchSongsWithContent"
 import HymnBookMeta from '../../ts/types/hymnBookMeta.interface';
 
-const MINI_HIDE_BOOK_HYMN = ["HC3", "CHC3", "H22", "H23", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10"]
+const MINI_HIDE_BOOK_HYMN = ["HC3", "CHC3", "H22", "H23"]
 
 const handle = async () => {
   const data = await getAllSongsWithContent();
